75957897fff66b2d6a867b354587aa7997600e07
LaNia2002 <estefania.ulloa@alumnos.ucn.cl>
2025-09-10 18:06:55 -0300

    Update Main.cpp

--- Main.cpp
+++ Main.cpp
@@ -2,7 +2,16 @@
 
 int main()
 {
-    
+    std::cout << "Bienvenido al menu: principal." << std::endl;
+	std::cout << "Ingrese la accion que quiere ejecutar: " << std::endl;
+	std::cout << "1) Gestion de alumnos" << std::endl;
+    std::cout << "2) Gestion de cursos" << std::endl;
+    std::cout << "3) Inscripcion de alumnos" << std::endl;
+    std::cout << "4) Gestion de notas" << std::endl;
+    std::cout << "5) Reportes" << std::endl;
+    int opcion;
+    std::cin >> opcion;
+    std::cout << opcion;
 
     return 0;
 }
