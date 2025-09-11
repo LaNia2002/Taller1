520f4fea669c1ae76af41ea6f1585d04b8c3073f
LaNia2002 <estefania.ulloa@alumnos.ucn.cl>
2025-09-10 23:10:49 -0300

    Update alumno.cpp

--- alumno.cpp
+++ alumno.cpp
@@ -1,36 +1,22 @@
 using namespace std;
 #include <iostream>
-#pragma once
+#include "alumno.h"
+#include <string.h>
 
-class alumno{
-    private: 
-        int id;
-        string nombre;
-        string apellido;
-        string carrera;
-        int diaIngreso;
-        int mesIngreso;
-        int anoIngreso;
-        alumno* sig;
-        
-        
-    public:
-        alumno::alumno(int id, string nombre, string carrera, int diaIngreso, int mesIngreso, int anoIngreso){
-            this -> id = id;
-            this -> nombre = nombre;
-            this -> carrera = carrera;
-            this -> diaIngreso = diaIngreso;
-            this -> mesIngreso = mesIngreso;
-            this -> anoIngreso = anoIngreso;
-            this -> sig = nullptr;
-        
-        int getID(){return id;}
-        string getNombre(){return nombre;}
-        string getApellido(){return apellido;}
-        int getDiaIngreso(){return diaIngreso;}
-        int getMesIngreso(){return mesIngreso;}
-        int getAnoIngreso(){return anoIngreso;}
-            
-        }
+    alumno::alumno(int id, string nombre, string carrera, int diaIngreso, int mesIngreso, int anoIngreso){
+        this -> id = id;
+        this -> nombre = nombre;
+        this -> carrera = carrera;
+        this -> diaIngreso = diaIngreso;
+        this -> mesIngreso = mesIngreso;
+        this -> anoIngreso = anoIngreso;
+        this -> sig = nullptr;
     }
-}
+        int alumno::getID() {return id;}
+        string alumno::getNombre(){return nombre;}
+        string alumno::getApellido(){return apellido;}
+        int alumno::getDiaIngreso(){return diaIngreso;}
+        int alumno::getMesIngreso(){return mesIngreso;}
+        int alumno::getAnoIngreso(){return anoIngreso;}
+            
+
