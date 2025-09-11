1c000d12f2f3ad493328e902b854afb5f27d8d83
Jossette12 <jossette.doll@alumnos.ucn.cl>
2025-09-11 19:28:27 -0300

    Create ListaAlumnos.h

---
+++ ListaAlumnos.h
@@ -0,0 +1,21 @@
+#include "alumno.h"
+#include <iostream>
+using namespace std;
+
+class NodoAlumno {
+public:
+    alumno dato;
+    NodoAlumno* sig;
+
+    NodoAlumno(alumno dato);   
+    ~NodoAlumno();         
+};
+
+class ListaAlumnos {
+private:
+    NodoAlumno* cabeza;
+
+public:
+    ListaAlumnos();
+    void insertar(alumno a);          
+};
