9bc90d146299057f5fd01b6a4c3dba9b958ae9da
Jossette12 <jossette.doll@alumnos.ucn.cl>
2025-09-11 20:14:28 -0300

    Update ListaCursos.h

--- ListaCursos.h
+++ ListaCursos.h
@@ -1,1 +1,21 @@
+#include "curso.h"
+#include <iostream>
+using namespace std;
 
+class NodoCursos {
+public:
+    curso dato;
+    NodoCursos* sig;
+
+    NodoCursos(curso dato);   
+    ~NodoCursos();         
+};
+
+class ListaCursos {
+private:
+    NodoCursos* cabeza;
+
+public:
+    ListaCursos();
+    void insertar(curso dato);          
+};
