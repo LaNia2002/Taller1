8e94f5d13ca42d9dc20b39cbb52f9dd055dcd1fa
Jossette12 <jossette.doll@alumnos.ucn.cl>
2025-09-11 10:50:50 -0300

    Update alumno.cpp

--- alumno.cpp
+++ alumno.cpp
@@ -3,9 +3,10 @@ using namespace std;
 #include "alumno.h"
 #include <string.h>
 
-    alumno::alumno(int id, string nombre, string carrera, int diaIngreso, int mesIngreso, int anoIngreso){
+    alumno::alumno(int id, string nombre,string apellido, string carrera, int diaIngreso, int mesIngreso, int anoIngreso){
         this -> id = id;
         this -> nombre = nombre;
+        this -> apellido = apellido;
         this -> carrera = carrera;
         this -> diaIngreso = diaIngreso;
         this -> mesIngreso = mesIngreso;
@@ -14,6 +15,7 @@ using namespace std;
         int alumno::getID() {return id;}
         string alumno::getNombre(){return nombre;}
         string alumno::getApellido(){return apellido;}
+        string alumno::getCarrera(){return carrera;}
         int alumno::getDiaIngreso(){return diaIngreso;}
         int alumno::getMesIngreso(){return mesIngreso;}
         int alumno::getAnoIngreso(){return anoIngreso;}
