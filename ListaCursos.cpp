9b286ed92c6ee37a29b07d287410b78b9ce6bba4
Jossette12 <jossette.doll@alumnos.ucn.cl>
2025-09-12 16:17:34 -0300

    Update ListaCursos.cpp

--- ListaCursos.cpp
+++ ListaCursos.cpp
@@ -17,4 +17,14 @@ void ListaCursos::insertar(curso dato) {
     NodoCursos* nuevo = new NodoCursos(dato);
     nuevo->sig = cabeza;
     cabeza = nuevo;
+
+ curso* ListaCursos::buscarPorCodigo(std::string codigo) {
+    NodoCursos* actual = cabeza;
+    while (actual != nullptr) {
+        if (actual->dato.getCodigo() == codigo) {
+            return &(actual->dato);
+        }
+        actual = actual->sig;
+    }
+    return nullptr;
 }
