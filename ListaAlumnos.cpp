958c4ca2f0c0a101293c09d536fbb74cc8b68e81
Jossette12 <jossette.doll@alumnos.ucn.cl>
2025-09-12 16:19:07 -0300

    Update ListaAlumnos.cpp

--- ListaAlumnos.cpp
+++ ListaAlumnos.cpp
@@ -17,4 +17,14 @@ void ListaAlumnos::insertar(alumno a) {
     NodoAlumno* nuevo = new NodoAlumno(a);
     nuevo->sig = cabeza;
     cabeza = nuevo;
+
+ alumno* ListaAlumnos::buscarPorID(int id) {
+    NodoAlumno* actual = cabeza;
+    while (actual != nullptr) {
+        if (actual->dato.getID() == id) {
+            return &(actual->dato);
+        }
+        actual = actual->sig;
+    }
+    return nullptr;
 }
