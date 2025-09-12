9f5b2e7b8488442c156c279ee263d8e10024ec2b
LaNia2002 <estefania.ulloa@alumnos.ucn.cl>
2025-09-11 22:33:13 -0300

    Create nota.cpp

---
+++ nota.cpp
@@ -0,0 +1,9 @@
+using namespace std;
+#include <iostream>
+#include "nota.h"
+#include <string.h>
+
+nota::nota(float data){
+        this -> data = data;
+    }
+        int data::getNota() {return data;}
