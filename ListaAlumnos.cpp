50bc150c7152752a6584078be0cc3300062f64d4
LaNia2002 <estefania.ulloa@alumnos.ucn.cl>
2025-09-12 21:32:11 -0300

    Update ListaAlumnos.cpp

--- ListaAlumnos.cpp
+++ ListaAlumnos.cpp
@@ -61,3 +61,5 @@ void ListaAlumnos::mostrarAlumno(string nombreAlumno) {
              << actual->dato.getAnoIngreso() << endl;}
         actual = actual->sig;
     }}
+NodoAlumno* ListaAlumnos::retornarCabeza(){
+    return cabeza;}
